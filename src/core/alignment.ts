bafbdf810bce4161b2fd0a9f25e5e570e5c0e311
Zeeshan Abid <zeeshan595@googlemail.com>
2019-05-25 23:53:50 +0100

    Alignment Error Checking

--- src/core/alignment.ts
+++ src/core/alignment.ts
@@ -16,6 +16,29 @@ export const toString = (align: EAlignment, name: string, type: "attribute" | "s
     return "";
   }
 
+  switch (name) {
+    case "align":
+    case "text-align":
+      if (
+        align == EAlignment.Top ||
+        align == EAlignment.Middle ||
+        align == EAlignment.Bottom
+      ) {
+        throw Error("You are trying to use vertical alignment on a field that only accepts horizontal aligments");
+      }
+      break;
+    case "valign":
+    case "vertical-align":
+      if (
+        align == EAlignment.Left ||
+        align == EAlignment.Center ||
+        align == EAlignment.Right
+      ) {
+        throw Error("You are trying to use horizontal alignment on a field that only accepts vertical alignments");
+      }
+      break;
+  }
+
   if (type == "attribute") {
     return ' ' + name + '="' + align.toString() + '" ';
   } else if (type == "style") {
